1c556f028cc790700f5b113ff5972388143a26be
Kevin <stoutkev3@gmail.com>
2017-02-20 09:06:56 -0500

    adding js to the header

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,18 +1,3 @@
-var $ = require('jquery');
-//var Person = require('./modules/Person');
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-  payTaxes(){
-    console.log(this.name + " now owes $0 in taxes");
-  }
-}
-
-var john = new Person("john doe", "blue");
-var jane = new Adult("jane smith", "orange");
-
-john.greet();
-jane.greet();
-jane.payTaxes();
-
-$("h1").remove();
+var mobileMenu = new MobileMenu();
