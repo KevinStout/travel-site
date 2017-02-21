db9789b44f4e8d60fd5563cea014fb1a78e07f96
Kevin <stoutkev3@gmail.com>
2017-02-20 21:31:58 -0500

    completed menu animation

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,3 +1,11 @@
-import MobileMenu from './modules/MobileMenu';
+var $ = require('jquery');
+var Person = require('./modules/Person');
 
-var mobileMenu = new MobileMenu();
+
+var john = new Person("john doe", "blue");
+var jane = new Person("jane smith", "green");
+
+john.greet();
+jane.greet();
+
+$("h1").remove();
