675c96ed18edaeeba09deb051bdc457200bfef00
Kevin <stoutkev3@gmail.com>
2017-02-25 10:11:10 -0500

    added the javascript to the modal page

---
+++ app/assets/scripts/modules/modal.js
@@ -0,0 +1,40 @@
+import $ from 'jquery';
+
+class Modal{
+  constructor(){
+    this.openModalButton = $('.open-modal');
+    this.modal = $(".modal");
+    this.closeModalButton = $(".modal__close");
+    this.events();
+  }
+
+  events(){
+    //clicking the open modal button
+    this.openModalButton.click(this.openModal.bind(this));
+
+    //clicking the x close modal button
+    this.closeModalButton.click(this.closeModal.bind(this));
+
+    //pushing the escape key
+    $(document).keyup(this.keyPressHandler.bind(this));
+  }
+
+  keyPressHandler(e){
+    if(e.keyCode == 27){
+      this.closeModal();
+    }
+  }
+
+  openModal(){
+    this.modal.addClass("modal--is-visible");
+
+    //this prevents the browser from scrolling up
+    return false;
+  }
+
+  closeModal(){
+    this.modal.removeClass("modal--is-visible");
+  }
+}
+
+export default Modal;
